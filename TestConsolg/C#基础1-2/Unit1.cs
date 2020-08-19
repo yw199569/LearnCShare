4fb7428fef1ad896189d5a0ec57df6fe9e76c669
yw199569 <yw199569@qq.com>
2020-08-19 22:24:42 +0800

    C#

--- TestConsolg/C#基础1-2/Unit1.cs
+++ TestConsolg/C#基础1-2/Unit1.cs
@@ -18,10 +18,11 @@
         //引用类型赋值是复制 了对象的引用。
         //引用类型在内存中需要为每一个对象单独分配空间，最少是8个字节，引用还需要额外占用字节
 
+        //byte ，sbyte,short,ushort 在进行运算的时候C#会按需对他们进行隐式转换，转换到大一点的整数类型。
         public void test()
         {
             short x = 1, y = 1;
-            //short z= x + y;  报错 因为short相加返回的是int类型
+            short z= x + y;  //报错 因为short相加C#duishort进行了隐式的转换，转换到了int类型，返回的是int类型
         }
 
 
@@ -41,6 +42,15 @@
         //checked操作符，运行的时候如果整形的表达式或者语句溢出，则会抛出overflowexception的错误
         //checked对float，double不起作用，因为他们有无限值。
 
+        //folat,double这俩个类型有特殊的值
+        //NaN
+        //+∞
+        //-∞
+        //-0
 
+
+        //NaN
+        //使用==时NaN不等于任何一个值，包括NaN
+        //使用object.Equals()时两个NaN是相等的
     }
 }
