35047f97a877bf9b4d97eb9103f78ccaa7d81b45
yangwen <yw199569@qq.com>
2020-11-25 22:24:01 +0800

    委托逆变

--- DepthInCShare/深入理解CShare/C#2/委托.cs
+++ DepthInCShare/深入理解CShare/C#2/委托.cs
@@ -8,6 +8,8 @@ namespace DepthInCShare.深入理解CShare.C2
     {
        
        delegate Stream StreamFactory();
+        delegate void StringFactory(string str);
+
        static MemoryStream GenerateSampleData()
        {
            byte[] buffer=new byte[16];
@@ -17,7 +19,7 @@ namespace DepthInCShare.深入理解CShare.C2
            }
            return new MemoryStream(buffer);
        }
-      
+      //委托的协变
        public  static void MainMethod()
         {
             StreamFactory factory=GenerateSampleData;//利用委托的协变来转换方法组
@@ -29,6 +31,15 @@ namespace DepthInCShare.深入理解CShare.C2
                     Console.WriteLine(data);
                 }
             }
+            //委托参数的逆变   ----没搞懂有啥用处？？？？？
+            StringFactory strFactory = ObjectSampleData;
+            strFactory.Invoke("qqq");
+        }
+
+        static void ObjectSampleData(object obj)
+        {
+
+            Console.WriteLine(obj.ToString());
         }
         
     }
